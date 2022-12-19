93ee1f8b6d22cb773c9cab8aee024b488a762a8b
RiccardoPellegrino <pellegrino.riccardo.90@gmail.com>
2022-12-19 16:45:24 +0100

    creati treni

--- database/seeders/TravelTrainSeeder.php
+++ database/seeders/TravelTrainSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\Trains;
+use App\Models\Train;
 use Faker\Generator as Faker;
 class TravelTrainSeeder extends Seeder
 {
@@ -15,18 +15,18 @@ class TravelTrainSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        for ($i = 0; $i < 10;$i++){
-        $newtrain = new Trains();
-        $newtrain -> azienda = $faker->words(2); 
-        $newtrain -> stazione_partenza = $faker->words(4); 
-        $newtrain -> stazione_arrivo = $faker->words(4); 
+        for ($i = 0; $i < 10; $i++){
+        $newtrain = new Train();
+        $newtrain -> azienda = $faker->name(); 
+        $newtrain -> stazione_partenza = $faker->city(); 
+        $newtrain -> stazione_arrivo = $faker->city(); 
         $newtrain -> orario_partenza = $faker->time(); 
         $newtrain -> orario_arrivo = $faker->time(); 
         $newtrain -> codice_treno = $faker->bothify(); 
         $newtrain -> numero_carrozze = $faker->randomDigit(); 
         $newtrain -> in_orario = $faker->boolean(); 
         $newtrain -> cancellato = $faker->boolean();
-            dd($newtrain);
+            $newtrain->save();
        }
     }
 }
